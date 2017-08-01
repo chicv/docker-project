42994707f3fecc21b8a273831bea034fee37d3e5
shiyongyong <shiyongyong@chicv.com>
2017-08-01 14:35:16 +0800

    启动时修改resolv

--- chicv/Cmd/startUp.sh
+++ chicv/Cmd/startUp.sh
@@ -3,3 +3,5 @@ sudo service nginx start
 sudo service php7.1-fpm start
 sudo service redis-server start
 sudo service  dnsmasq start
+
+sudo cp /etc/resolv.conf /etc/resolv.conf.bak && sudo sed -i "s/^nameserver\ 127\.0\.0\.11/nameserver\ 127.0.0.1/" /etc/resolv.conf.bak && sudo cp /etc/resolv.conf.bak /etc/resolv.conf
